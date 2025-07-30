eaf4705b43dcd1e1cb5376833a9c79ad45bb8f1f
tigeryanggit <tiger_yanghk@yahoo.com.hk>
2025-07-30 15:34:32 +0800

    keyframes

---
+++ js/toggle.js
@@ -0,0 +1,11 @@
+
+
+const button = document.querySelector("#btn-1");
+
+const heading = document.querySelector("h1");
+
+button.addEventListener("click",()=> {
+    document.body.classList.toggle("dark");
+    heading.classList.toggle("yellow");
+});
+
